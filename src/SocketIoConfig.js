e171f21fc68bcd097596f70d21f3f50f8a3688e4
Hoang Long <longch@edmicro.vn>
2021-01-13 23:04:12 +0700

    update

--- src/SocketIoConfig.js
+++ src/SocketIoConfig.js
@@ -1,9 +1,9 @@
 import io from 'socket.io-client';
 const urlLocal = 'http://localhost:3000';
 const urlOnline = 'https://url3k.sse.codesandbox.io/';
-
+const urlHeroku = 'https://backend-ddtank.herokuapp.com/';
 function SocketIoConfig() {
-    const socket = io(urlOnline);
+    const socket = io(urlHeroku);
     return socket;
 }
 
